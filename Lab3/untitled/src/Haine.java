015b6bb267701392d74934db3677334bd02fa860
CiocanRazvan <ciocanrazvan4@gmail.com>
2026-03-05 09:20:26 +0200

    Delete Lab3

--- Lab3/untitled/src/Haine.java
+++
@@ -1,8 +0,0 @@
-public class Haine extends Produse{
-    private String producator;
-
-    public Haine(String id, String titlu,String producator,double cant) {
-        super(id,titlu,cant);
-        this.producator = producator;
-    }
-}
