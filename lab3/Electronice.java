1c27ca5ae7fcd82bb74c856a87893f1870560f7c
WhiterGS1 <pmih.109@gmail.com>
2026-03-12 09:25:46 +0200

    Folder diagrame

--- lab3/Electronice.java
+++
@@ -1,8 +0,0 @@
-public class Electronice extends Produse{
-    private String producator;
-
-    public Electronice(String id, String titlu, String producator){
-        super(id,titlu);
-        this.producator = producator;
-    }
-}
--- lab3/Haine.java
+++
@@ -1,8 +0,0 @@
-public class Haine extends Produse{
-    private String producator;
-
-    public Haine(String id, String titlu,String producator) {
-        super(id,titlu);
-        this.producator = producator;
-    }
-}
